10ecbc76f0fa3fe6c2f1c9781299a1f18774fe82
Kevin Wong <1996kevinwong@gmail.com>
2018-03-30 05:26:17 -0400

    Milestone Meeting 2
    Score and inventory system that updates when player colects truffles.
    Can drop off truffles into chests. Pig AI that chases player.

--- Pigs In A Blanket/Assets/Scripts/PlayerMovement.cs
+++ Pigs In A Blanket/Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement: MonoBehaviour {
 
 	public float speed;
+
+
+    public Text scoreText;
+    public Text truffleCount;
+    public Text inventoryWeight;
+
+    public int score = 0;
+    public int weight = 0;
+
+    public int numTruffles = 0;
 
 	private Rigidbody2D rb2d;
 
@@ -18,4 +29,34 @@ public class PlayerMovement : MonoBehaviour {
 		Vector2 movement = new Vector2 (moveHorizontal,moveVertical);
 		rb2d.AddForce (movement * speed);
 	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag ("Truffle"))
+        {
+            numTruffles++;
+            weight++;
+            inventoryWeight.text = weight + "/100";
+            truffleCount.text = numTruffles.ToString();
+            other.gameObject.SetActive(false);
+        }
+
+        if (other.gameObject.CompareTag("Chest"))
+        {   
+            score += numTruffles;
+            numTruffles = 0;
+            weight = 0;
+            inventoryWeight.text = weight + "/100";
+            truffleCount.text = numTruffles.ToString();
+            UpdateScore();
+
+        }
+
+    }
+
+
+    void UpdateScore()
+    {   
+        scoreText.text = "Score: " + score;
+    }
 }
---
+++ Pigs In A Blanket/Assets/Scripts/timer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class timer : MonoBehaviour {
+	public Text timerLabel;
+
+	private float time;
+	private float timeRemaining;
+	private float allowedTime = 240;
+	private int allowedMinutes = 240 / 60;
+	void Update() {
+		time += Time.deltaTime;
+		allowedTime -= Time.deltaTime;
+		var minutesRemaining = allowedMinutes - (allowedMinutes - (allowedTime / 60));
+		var secondsRemaining = allowedTime % 60;
+
+		//update the label value
+		if (secondsRemaining > 10)
+			timerLabel.text = string.Format("Time: {0,1:F0} : {1,2:F2}", Math.Ceiling(minutesRemaining) - 1, secondsRemaining);
+		else
+			timerLabel.text = string.Format("Time: {0,1:F0} : 0{1,2:F2}", Math.Ceiling(minutesRemaining) - 1, secondsRemaining);
+	}
+}
